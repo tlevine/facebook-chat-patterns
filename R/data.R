f30a9adef99de1bb904aa818b6e744ad211da381
Thomas Levine <_@thomaslevine.com>
2014-12-13 02:44:11 +0000

    move stuff for packaging

--- R/data.R
+++ R/data.R
@@ -1,12 +1,10 @@
 queries <- list(
   status.changes = '
-SELECT date(datetime) \'date\',
-       user_id \'user.id\',
-       count(*) \'status.changes\'
+SELECT date(datetime) "date",
+       user_id "user.id",
+       count(*) "status.changes"
 FROM ft_facebookchatstatuschange
 GROUP BY date, "user.id"',
-  online.duration = '
-SELECT date, user_id \'user.id\', duration
-FROM ft_facebookduration')
+  online.duration = 'SELECT date, user_id "user.id", duration FROM ft_facebookduration')
 
 q <- function(sql) sqldf(sql, drv = 'PostgreSQL', user = 'tlevine', dbname = 'tlevine')
--- README.md
+++
@@ -1,4 +0,0 @@
-I find that I go on Facebook more when I'm more lonely.
-I wonder whether other people do the same. And regardless
-of people's reasons for having different Facebook usage
-patterns, it might be interesting to find them.
